c61bda6f3a40fe6dbac9f469098102fb18a11b38
Ricardo Erikson <ricardoerikson@gmail.com>
2020-11-21 16:36:21 -0300

    build(gha-workflows): wip: add workflows to build the project

--- release.config.js
+++ release.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  "branches": ["develop"],
+  "ci": false,
+  "branches": ["main"],
   "plugins": [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     ["@semantic-release/changelog", {
       "changelogFile": "CHANGELOG.md"
+    }],
+    ["@semantic-release/exec", {
+      "prepareCmd": "echo 'Version: ${nextRelease.version}'"
     }]
   ]
 
