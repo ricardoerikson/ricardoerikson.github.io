c3e6388d7915698db64ec83fba9e74cbcc9c33b5
Ricardo Erikson <ricardoerikson@gmail.com>
2020-11-22 11:29:09 -0300

    build(analytics-replacer): fix index.html path

--- analytics-replacer.js
+++ analytics-replacer.js
@@ -1,7 +1,7 @@
 const replace = require('replace-in-file');
 
 const options = {
-  files: 'dist/gh-pages/index.html',
+  files: 'dist/index.html',
   from: '{{GOOGLE_ANALYTICS}}',
   to: process.env.GOOGLE_ANALYTICS
 }
