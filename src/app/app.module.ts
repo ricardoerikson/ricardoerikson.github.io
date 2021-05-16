37a133d628d7fbfeb1e2f171f53255644d2fdc5d
Ricardo Erikson <ricardoerikson@gmail.com>
2021-05-16 16:26:04 -0300

    move footer to core folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,33 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { SectionContentComponent } from './core/section-content/section-content.component';
+import { SectionComponent } from './core/section/section.component';
 import { TitleComponent } from './core/title/title.component';
-import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { IconComponent } from './icon/icon.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { SectionComponent } from './core/section/section.component';
-import { SectionContentComponent } from './core/section-content/section-content.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    FooterComponent,
-    IconComponent,
-    AboutComponent,
-    TitleComponent,
-    SectionComponent,
-    SectionContentComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        HomeComponent,
+        FooterComponent,
+        IconComponent,
+        AboutComponent,
+        TitleComponent,
+        SectionComponent,
+        SectionContentComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
