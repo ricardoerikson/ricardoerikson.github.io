8bfdc5d4751023b71e5ec6845bfd58552b31a98a
Ricardo Erikson <ricardoerikson@gmail.com>
2021-05-16 17:23:14 -0300

    move navbar component to the core folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
 import { SectionContentComponent } from './core/section-content/section-content.component';
 import { SectionComponent } from './core/section/section.component';
 import { TitleComponent } from './core/title/title.component';
 import { HomeComponent } from './home/home.component';
 import { IconComponent } from './icon/icon.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
